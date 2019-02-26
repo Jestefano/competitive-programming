41928db6533f5db01676168e76041cca895f8702
Jesus Torres <jstorres@pucp.pe>
2019-02-26 12:18:12 -0500

    Macros
    This are the main macros that we are going to use

---
+++ week-2/macros.cpp
@@ -0,0 +1,25 @@
+//macros!
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX 100
+#define FOR(i,n,m) for(int i = (n); i < (m); i++)
+#define REP(i,n) FOR(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define all(v) v.begin(),v.end()
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+
+
+int main(){
+	
+
+	return 0;
+}
