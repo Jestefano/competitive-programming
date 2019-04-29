a06c54460bc233e720ce50d95bb40dc2a169b0c9
Jesus Torres <jstorres@pucp.pe>
2019-04-29 14:34:22 -0500

    Temporal implementation of BLMN FORD

---
+++ algorithms/graphs/bellman_ford/bellman_ford.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nax 100
+#define pb push_back
+#define FOR(i,n,m) for(int i =(n);i<(m);i++)
+#define REP(i,n) FOR(i,0,n)
+#define fst first
+#define snd second
+#define mp make_pair
+
+typedef pair<int,int> ii;
+
+int INF = 1000, p[nax], d[nax];
+vector<pair<int,ii> > edges;
+bool neg_cicle = false;
+
+void bellman_ford(int n, int m, int s){
+	REP(i,n) d[i] = INF;
+	d[s] = 0;
+	p[s] = -1;
+	REP(i,n-1){
+		REP(j,m){
+			int w = edges[j].fst;
+			int node1 = edges[j].snd.fst, node2 = edges[j].snd.snd;
+			if(d[node1]==INF) continue;
+			if(d[node1]+w<d[node2]){
+				d[node2] = d[node1] + w;
+				p[node2] = node1;
+			}
+		}
+	}
+
+	REP(j,m){
+		int w = edges[j].fst;
+		int node1 = edges[j].snd.fst, node2 = edges[j].snd.snd;
+		if(d[node1]==INF) continue;
+		if(d[node1]+w<d[node2]){
+			cout<<"?";
+			neg_cicle = true;
+		}
+	}
+}
+
+int main(){
+	int n,m; cin>>n>>m;
+	REP(i,n){
+		int a,b,c; cin>>a>>b>>c;
+		a--;b--;
+		edges.pb(mp(c,mp(a,b)));
+	}
+	bellman_ford(n,m,0);
+	
+	REP(i,n){
+		cout<<d[i]<<endl;
+	}
+	cout<<neg_cicle<<endl;
+
+	return 0;
+}
